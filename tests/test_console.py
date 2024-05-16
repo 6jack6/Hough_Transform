0ef22addecf04a855e06a0623918f80c7b2302a6
6jack6 <tsyrentarov.as@phystech.edu>
2024-05-16 23:37:56 +0300

    Some fixes for tests

--- tests/test_console.py
+++ tests/test_console.py
@@ -39,7 +39,7 @@ def test_image_processor_init() -> None:
 
 def test_image_processor_resize_to_const() -> None:
     """Test ImageProcessor resize to const."""
-    img = Image.open('images/horizontal_lines.png')
+    img = Image.open('../images/horizontal_lines.png')
     img_processor = ImageProcessor('')
     new_img = img_processor.resize_to_const(img)
     m = max(new_img.size)
@@ -52,14 +52,14 @@ def test_image_processor_resize_to_const() -> None:
 
 def test_image_processor_calc_sums() -> None:
     """Test ImageProcessor calc_sums."""
-    img = np.array(Image.open('images/horizontal_lines.png').convert("L"))
+    img = np.array(Image.open('../images/horizontal_lines.png').convert("L"))
     img_processor = ImageProcessor('')
-    res = img_processor.calc_sums(img, img.shape[2], img.shape[1], img.shape[0])
+    res = img_processor.calc_sums(img, 0, 5, img.shape[0])
     assert isinstance(res, np.ndarray)
 
 
 def test_image_processor_process_image() -> None:
     """Test ImageProcessor process_image."""
-    img_processor = ImageProcessor('images/horizontal_lines.png')
+    img_processor = ImageProcessor('../images/horizontal_lines.png')
     color_img = img_processor.process_image()
     assert isinstance(color_img, Image.Image)
